4cd49ee9a5933b4b29bbc2b8d1a36a42674085f4
Gsschenker <gschenke@villanova.edu>
2023-12-04 15:26:26 -0500

    added firebase.js and created access point from App.js

---
+++ nova-gas/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAFCGCK7WOnKcc_QMSRBaChkc17ah_skic",
+  authDomain: "novagas.firebaseapp.com",
+  projectId: "novagas",
+  storageBucket: "novagas.appspot.com",
+  messagingSenderId: "1075062940905",
+  appId: "1:1075062940905:web:8a3a03890c7dead58ebc78",
+  measurementId: "G-20G0FMNWG2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
