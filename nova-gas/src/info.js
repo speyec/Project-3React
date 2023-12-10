27b4c481c6edeb2d49f242ee052a852d4a096aed
speyec <ouatum44@gmail.com>
2023-12-10 13:37:48 -0500

    files

--- nova-gas/src/info.js
+++ nova-gas/src/info.js
@@ -1,7 +1,9 @@
 import React, {useState, useEffect} from "react";
+import { StyleSheet} from 'react'
 import { firestore } from "./firebase";
 import "firebase/firestore";
 import { collection, getDocs, query } from "firebase/firestore";
+import './GasStations.css';
 
   export default function GasStations() {
     const [gasStations, setGasStations] = useState([]);
@@ -38,4 +40,24 @@ import { collection, getDocs, query } from "firebase/firestore";
     </div>
   );
 }
+
+// const styles = StyleSheet.create({
+//   container: {
+//    paddingTop: 50
+//   },
+//   itemName: {
+//     padding: 10,
+//     fontSize: 20,
+//     height: 40,
+//   },
+//   item: {
+//     padding: 10,
+//     fontSize: 24,
+//     height: 40,
+//   },
+//   border: {
+//     borderWidth: 1,
+//     borderColor: "gray",
+//   }
+// }); 
 
